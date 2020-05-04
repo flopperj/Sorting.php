f8c406b2a54b24de1f9638c26b568eee822b9fc9
jamesarama <james@funeralinnovations.com>
2020-05-03 23:03:27 -0700

    - added merge sort implementation

--- sorting.php
+++ sorting.php
@@ -15,6 +15,54 @@ function swap($start, $end, $arr)
 }
 
 /**
+ * Merges two sorted arrays
+ * Time complexity = O(n)
+ *
+ * @param array $left
+ * @param array $right
+ * @return array
+ */
+function merge($left = [], $right = [])
+{
+
+    // initialize empty result array
+    $result = [];
+
+    // initialize leftIndex = 0
+    $leftIndex = 0;
+
+    // initialize rightIndex = 0
+    $rightIndex = 0;
+
+    // loop through left and num why leftIndex < sizeof(left) and rightIndex < sizeof(right)
+    while ($leftIndex < sizeof($left) && $rightIndex < sizeof($right)) {
+
+        // check if left[leftIndex] < right[rightIndex]
+        if ($left[$leftIndex] < $right[$rightIndex]) {
+
+            // push left[leftIndex] to results array
+            $result[] = $left[$leftIndex];
+
+            // increment leftIndex
+            $leftIndex++;
+        } else {
+            // else right[rightIndex] < left[leftIndex]
+            // push right[rightIndex] to results array
+            $result[] = $right[$rightIndex];
+
+            // increment rightIndex
+            $rightIndex++;
+        }
+    }
+
+    // merge remaining left and right array to result
+    $result = array_merge($result, array_slice($left, $leftIndex));
+    $result = array_merge($result, array_slice($right, $rightIndex));
+
+    return $result;
+}
+
+/**
  * Implements bubble sort algorithm
  * Time Complexity = O(n^2)
  *
@@ -58,7 +106,7 @@ function bubbleSort($nums = [])
 function insertionSort($nums = [])
 {
     // loop through nums from i = 1 to size of nums
-    for($i = 1; $i < sizeof($nums); $i++){
+    for ($i = 1; $i < sizeof($nums); $i++) {
 
         // set our key
         $key = $nums[$i];
@@ -66,8 +114,8 @@ function insertionSort($nums = [])
         // set previous compareIndex = i - 1;
         $compareIndex = $i - 1;
 
-        // now loop to check all previous elements while compareIndex >= 0 and key > nums[compareIndex]
-        while($compareIndex >= 0 && $nums[$compareIndex] > $key){
+        // now loop to check all previous elements while compareIndex >= 0 and nums[compareIndex] > key
+        while ($compareIndex >= 0 && $nums[$compareIndex] > $key) {
 
             // swap compareIndex with compareIndex + 1 elements
             $nums = swap($compareIndex, $compareIndex + 1, $nums);
@@ -80,8 +128,36 @@ function insertionSort($nums = [])
     return $nums;
 }
 
+/**
+ * Implementation of the mergeSort algorithm
+ * Time complexity = O(nlogn)
+ *
+ * @param array $nums
+ * @return array
+ */
 function mergeSort($nums = [])
 {
+    // return nums if has 1 element or is empty
+    if (sizeof($nums) <= 1) {
+        return $nums;
+    }
+
+    // find mid point to help split array in two halves
+    $mid = floor(sizeof($nums) / 2);
+
+    // get left side
+    $left = array_slice($nums, 0, $mid);
+
+    // get right side
+    $right = array_slice($nums, $mid);
+
+    // return merge
+    return merge(
+    // mergeSort on left
+        mergeSort($left),
+        // mergeSort on right
+        mergeSort($right)
+    );
 }
 
 // TEST CASES
@@ -108,16 +184,21 @@ foreach ($tests as $test_case) {
 
     // test bubble sort
     $bubble_sort_test = bubbleSort($test_case['test']);
-    print 'Testing bubbleSort('.$test_value.') => ' . $accepted_value;
+    print 'Testing bubbleSort(' . $test_value . ') => ' . $accepted_value;
     print "\n";
-    print "result=" . ($bubble_sort_test === $test_case['accepted'] ? 'Passed' : 'Failed with ' . print_r($bubble_sort_test, true) );
+    print "result=" . ($bubble_sort_test === $test_case['accepted'] ? 'Passed' : 'Failed with ' . print_r($bubble_sort_test, true));
     print "\n\n";
 
     // test merge sort
+    $merge_sort_test = mergeSort($test_case['test']);
+    print 'Testing mergeSort(' . $test_value . ') => ' . $accepted_value;
+    print "\n";
+    print "result=" . ($merge_sort_test === $test_case['accepted'] ? 'Passed' : 'Failed with ' . print_r($merge_sort_test, true));
+    print "\n\n";
 
     // test insertion sort
     $insertion_sort_test = insertionSort($test_case['test']);
-    print 'Testing insertionSort('.$test_value.') => ' . $accepted_value;
+    print 'Testing insertionSort(' . $test_value . ') => ' . $accepted_value;
     print "\n";
     print "result=" . ($insertion_sort_test === $test_case['accepted'] ? 'Passed' : 'Failed with ' . print_r($insertion_sort_test, true));
     print "\n\n";
